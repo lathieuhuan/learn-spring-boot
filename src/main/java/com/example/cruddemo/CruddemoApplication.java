f479a753c9244a710e0786f6b420afe37833ec7e
lathieuhuan <huan.la@onemount.com>
2025-02-03 18:10:22 +0700

    update

--- src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -17,8 +19,45 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+			createStudents(studentDAO);
+//			queryStudents(studentDAO);
+//			queryStudentsByLastname(studentDAO);
+//			updateStudent(studentDAO);
+//			deleteStudent(studentDAO);
+//			deleteAllStudents(studentDAO);
 		};
+	}
+
+	private void deleteAllStudents(StudentDAO studentDAO) {
+		int deletedStudentCount = studentDAO.deleteAll();
+		System.out.println("Number of deleted Students " + deletedStudentCount);
+	}
+
+	private void deleteStudent(StudentDAO studentDAO) {
+		studentDAO.delete(1);
+	}
+
+	private void updateStudent(StudentDAO studentDAO) {
+		Student student = studentDAO.findById(1);
+		student.setFirstName("Janes");
+		studentDAO.update(student);
+	}
+
+	private void queryStudentsByLastname(StudentDAO studentDAO) {
+		List<Student> students = studentDAO.findByLastName("Doe");
+
+		for (Student student: students) {
+			System.out.println(student);
+		}
+	}
+
+	private void queryStudents(StudentDAO studentDAO) {
+		List<Student> students = studentDAO.findAll();
+
+		for (Student student: students) {
+			System.out.println(student);
+		}
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
@@ -28,4 +67,15 @@ public class CruddemoApplication {
 
 		System.out.println("Saved student:" + student.getId());
 	}
+
+	private void createStudents(StudentDAO studentDAO) {
+		Student student1 = new Student("Jane", "Doe", "jane.doe@gmail.com");
+		Student student2 = new Student("John", "Smith", "john.smith@gmail.com");
+		Student student3 = new Student("Dean", "Winchester", "dean.winchester@gmail.com");
+
+		studentDAO.save(student1);
+		studentDAO.save(student2);
+		studentDAO.save(student3);
+	}
+
 }
--- src/main/java/com/example/cruddemo/dao/StudentDAOImpl.java
+++ src/main/java/com/example/cruddemo/dao/StudentDAOImpl.java
@@ -2,11 +2,14 @@ package com.example.cruddemo.dao;
 
 import com.example.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
-public class StudentDAOImpl implements  StudentDAO {
+public class StudentDAOImpl implements StudentDAO {
 
     private EntityManager entityManager;
 
@@ -18,5 +21,45 @@ public class StudentDAOImpl implements  StudentDAO {
     @Transactional
     public void save(Student student) {
         entityManager.persist(student);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+//        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName desc", Student.class);
+
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:lastName", Student.class);
+        theQuery.setParameter("lastName", lastName);
+        return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student student) {
+        entityManager.merge(student);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        Student student = entityManager.find(Student.class, id);
+        entityManager.remove(student);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        int deletedRowCount = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+        return deletedRowCount;
     }
 }
